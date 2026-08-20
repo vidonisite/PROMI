350a50aa90b20f91d6bb362389a24070f51caa5d
vidonisite <joni.zernell@proton.me>
2026-08-20 18:36:07 +0200

    Replace home icon with SVG in App component

--- src/App.jsx
+++ src/App.jsx
@@ -985,7 +985,14 @@ function App() {
             setActiveTab("bo")
           }
         >
-          <span>🏠</span>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-home-2">
+            	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            	<path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+            	<path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+            	<path d="M10 12h4v4h-4l0 -4" />
+            </svg>
+          </span>
           <small>Bo</small>
         </button>
 
